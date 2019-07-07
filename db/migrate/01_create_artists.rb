530dd73f4fac17298e0a646beb3f2d079a4530db
Amanda Treutler <amandathedev@gmail.com>
2019-07-07 11:56:36 -0500

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,62 @@
+mechanics-of-migrations-v-000/
+  config/
+    environment.rb
+  db/
+    migrate/
+      01_create_artists.rb
+  spec/
+    artist_spec.rb
+    spec_helper.rb
+  .gitignore
+  .learn
+  .rspec
+  artist.rb
+  CONTRIBUTING.md
+  Gemfile
+  Gemfile.lock
+  LICENSE.md
+  Rakefile
+  README.md
+
+  class CreateArtists < ActiveRecord::Migration
+    def up
+    end
+   
+    def down
+    end
+  end
+
+#   ActiveRecord::Base.establish_connection(
+#     :adapter => "sqlite3",
+#     :database => "db/artists.sqlite"
+#   )
+
+
+#   sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS artists (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+ 
+# ActiveRecord::Base.connection.execute(sql)
+
+
+def change
+    create_table :artists do |t|
+    end
+end
+
+class CreateArtists < ActiveRecord::Migration
+    def change
+      create_table :artists do |t|
+        t.string :name
+        t.string :genre
+        t.integer :age
+        t.string :hometown
+      end
+    end
+  end
